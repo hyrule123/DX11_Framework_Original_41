6f1915552e6bc08511e394d879e6d5971e1500c6
ekdrn1 <ekdrn1@naver.com>
2022-07-20 16:34:43 +0900

    초기 커밋.

---
+++ AR41Engine/Include/Engine.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "EngineInfo.h"
+
+class CEngine
+{
+private:
+	HINSTANCE	m_hInst;
+	HWND		m_hWnd;
+	Resolution	m_WindowRS;
+	static bool	m_Loop;
+
+public:
+	Resolution GetWindowResolution()	const
+	{
+		return m_WindowRS;
+	}
+
+	HWND GetWindowHandle()	const
+	{
+		return m_hWnd;
+	}
+
+	HINSTANCE GetWindowInstance()	const
+	{
+		return m_hInst;
+	}
+
+public:
+	bool Init(HINSTANCE hInst, const TCHAR* Title,
+		const TCHAR* ClassName, int IconID, int SmallIconID,
+		unsigned int WindowWidth,
+		unsigned int WindowHeight, 
+		unsigned int DeviceWidth, unsigned int DeviceHeight,
+		bool WindowMode = true);
+	int Run();
+
+private:
+	void Logic();
+	void Input(float DeltaTime);
+	bool Update(float DeltaTime);
+	bool PostUpdate(float DeltaTime);
+	void Collision(float DeltaTime);
+	void Render(float DeltaTime);
+
+
+private:
+	void Register(const TCHAR* ClassName, int IconID, int SmallIconID);
+	bool Create(const TCHAR* Title, const TCHAR* ClassName);
+
+private:
+	static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+	DECLARE_SINGLE(CEngine)
+};
+
